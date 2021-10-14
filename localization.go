7676106ec63a4226b2e191eb7327041222d6c592
SpiritOfSim <spiritofsim@yandex.ru>
2021-10-14 08:30:46 +0300

    Fix localization

--- localization.go
+++ localization.go
@@ -2,7 +2,7 @@ package main
 
 const (
 	VisitPlatformText = "Visit platform %v"
-	TransferCargoText = "Transfer %v to %v"
+	TransferCargoText = "Transfer cargo %v to platform %v"
 	DistanceText      = "%vm"
 	FuelLabelText     = "FUEL"
 	EnergyLabelText   = "ENERGY"
